d0f0dae21379fd40c47208a5194087c1b3082da7
alijarai12 <alijarai414@gmail.com>
2022-12-05 16:43:47 +0545

    first commit

---
+++ includes/brands-slider.php
@@ -0,0 +1,42 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<!-- <h3 class="section-title">Online Payment </h3> -->
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ee.png" width="90" height="40" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/kk.png" width="90" height="40" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/p.jpg" width="90" height="40" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ip.jpg" width="90" height="40" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/ip.png" width="90" height="40" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
